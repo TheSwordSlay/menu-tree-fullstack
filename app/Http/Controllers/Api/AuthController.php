35bdce05239a0c825a240bd52ea88007bc75b52c
Rizalul Fiqri <107758410+TheSwordSlay@users.noreply.github.com>
2025-08-11 02:06:05 +0700

    added register api endpoint

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -40,9 +40,6 @@ class AuthController extends Controller
         }
     }
 
-    /**
-     * Log the user out (revoke the token).
-     */
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
@@ -50,5 +47,33 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Logged out successfully'
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'message' => 'User registered successfully.',
+                'token' => $user->createToken('api-token')->plainTextToken
+            ], 201);
+
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => $e->errors(),
+            ], 422);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,16 @@ use App\Http\Controllers\MenuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Public route for getting a token
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
-// Protected routes that require a valid Sanctum token
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    // Add this route for logging out
     Route::post('/logout', [AuthController::class, 'logout']);
-
-    // Menu API routes
+    
     Route::get('/menu/get-all', [MenuController::class, 'apiGetAllMenus']);
     Route::post('/menu/create', [MenuController::class, 'apiCreate']);
     Route::post('/menu/create-child', [MenuController::class, 'apiCreateChild']);
